fd2adddeb55f55c9f010b021756edf162c4349bb
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-03-30 12:33:09 +0330

    server sided

--- app/(front)/sticker-packs/[categoryName]/[stickerName]/page.jsx
+++ app/(front)/sticker-packs/[categoryName]/[stickerName]/page.jsx
@@ -1,35 +1,42 @@
-"use client";
 import Stickers from "../Stickers";
 import HeaderSticker from "./HeaderSticker";
 import StickerPage from "./Sticker";
-import axios from "axios";
-import { useState, useEffect } from "react";
 
-function StickerSinglePage({ params }) {
-  const [sticker, setSticker] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    axios
-      .get(
-        `${process.env.NEXT_PUBLIC_SRV_URL}/getStickerByName/${params.stickerName}`
-      )
-      .then((res) => {
-        setSticker(res.data[0]);
-        setLoading(false);
-      });
-  }, [loading]);
+// to show 404 for pack names that doesnt exist
+export const dynamicParams = false;
+
+const generateStaticParams = async () => {
+  const stickers = await fetch(
+    `${process.env.NEXT_PUBLIC_SRV_URL}/allStickers`
+  );
+  return stickers.map((stickers) => ({
+    categoryName: stickers.pack,
+    stcikerName: stickers.title,
+  }));
+};
+
+const getSticker = async (stickerName) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SRV_URL}/getStickerByName/${stickerName}`
+  );
+  const data = await res.json();
+  return data[0];
+};
+async function StickerSinglePage({ params }) {
+  const packname = params.categoryName;
+  const stickername = params.stickerName;
+  const sticker = await getSticker(stickername);
+
   return (
     <>
       <HeaderSticker color={sticker.color} textColor={"black"} />
       <div className="flex flex-col justify-center items-center">
-        {!loading && (
-          <StickerPage
-            title={params.stickerName}
-            pack={params.categoryName}
-            targetsticker={sticker}
-          />
-        )}
-        <Stickers title={params.categoryName} />
+        <StickerPage
+          title={stickername}
+          pack={packname}
+          targetsticker={sticker}
+        />
+        <Stickers title={packname} />
       </div>
     </>
   );
