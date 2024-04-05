6493f7e1156be43334a95886d916ea9d6024d270
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-05 19:17:59 +0330

    fetch data

--- app/(front)/stickers/Stickers.jsx
+++ app/(front)/stickers/Stickers.jsx
@@ -1,30 +1,30 @@
 "client component";
-import Link from "next/link";
-import Image from "next/image";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import Pagination from "@/app/ui/Pagination";
 import { useSearchParams } from "next/navigation";
 import Stickers from "@/app/(front)/ui/Stickers";
 
+const getStickers = async (pagenumber, pagination) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SRV_URL}/allStickers?pn=${pagenumber}&pg=${pagination}`
+  );
+  return res.json();
+};
+
 function StickersPage() {
   const searchParams = useSearchParams();
   const [stickers, setStickers] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [allStickerQty, setAllStickerQty] = useState();
   const pagenumber = Number(searchParams.get("pn")) || 1;
   const pagination = Number(searchParams.get("pg")) || 30;
   useEffect(() => {
-    axios
-      .get(
-        `${process.env.NEXT_PUBLIC_SRV_URL}/allStickers?pn=${pagenumber}&pg=${pagination}`
-      )
-      .then((res) => {
-        setStickers(res.data.paginatedStickers);
-        setAllStickerQty(res.data.qty);
-        setLoading(false);
-      });
-  }, [loading, pagenumber, pagination]);
+    const fetchData = async () => {
+      const data = await getStickers(pagenumber, pagination);
+      setStickers(data.paginatedStickers);
+      setAllStickerQty(data.qty);
+    };
+    fetchData();
+  }, [pagenumber, pagination]);
   return (
     <>
       <Stickers stickers={stickers} />
