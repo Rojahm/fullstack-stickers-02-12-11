38e0659fa685e5999ca31b9f69e10d3e7740afca
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-05 12:51:23 +0330

    new not found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,35 @@
-function notFound() {
+import Image from "next/image";
+import Link from "next/link";
+// UI icons
+import { CiHome } from "react-icons/ci";
+
+function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="bg-sky-300 h-[100vh] w-full flex flex-col items-center">
+      <div className="flex items-center gap-5 mt-10 mb-16">
+        <Link href={"/"}>
+          <CiHome size={44} />
+          <p className="font-bold">Home</p>
+        </Link>
+        <Link href={"/"}>
+          <Image
+            src={"/notFound (2).png"}
+            width={150}
+            height={150}
+            alt="go home gary"
+          />
+        </Link>
+      </div>
+      <Link href={"/"}>
+        <Image
+          src={"/notFound (1).png"}
+          width={300}
+          height={300}
+          alt="page not found ghost"
+        />
+      </Link>
     </div>
   );
 }
 
-export default notFound;
+export default NotFound;
