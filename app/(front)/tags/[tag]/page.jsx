93b31af27c6410c0cd4df76ab4ec00e2afb04463
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-06 14:52:54 +0330

    tag page added

--- app/(front)/tags/[tag]/page.jsx
+++ app/(front)/tags/[tag]/page.jsx
@@ -1,12 +1,37 @@
+"use client";
 import PackPageHero from "../../sticker-packs/PackPageHero";
+import Stickers from "../../ui/Stickers";
+import { useEffect, useState } from "react";
+
+// export const dynamicParams = true;
+
+const getStickers = async (tag) => {
+  const result = await fetch(
+    `${process.env.NEXT_PUBLIC_SRV_URL}/getStickersByTag/${tag}`
+  );
+  // console.log(result.json());
+  return result.json();
+};
 
 function TagPage({ params }) {
+  // const stickers = await getStickers(params.tag);
+  const [stickers, setStickers] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getStickers(params.tag);
+      setStickers(data);
+    };
+    getData();
+  }, []);
   return (
     <>
-      <PackPageHero title={`STICKERS OF ${params.tag}`} />
+      <PackPageHero title={`STICKERS OF ${params.tag.split("-").join(" ")}`} />
       <div className="my-10 px-6 md:px-12 lg:px-40 flex flex-col">
-        <h1 className="text-2xl">Stickers on the subject of {params.tag}</h1>
+        <h1 className="text-2xl capitalize">
+          Stickers on the subject of {params.tag.split("-").join(" ")}
+        </h1>
         <hr className="border-[#814997] border-[3px] rounded-md" />
+        <Stickers stickers={stickers} />
       </div>
     </>
   );
