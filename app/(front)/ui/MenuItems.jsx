713b4c6d533d4219f81c702b22f71568ccab7ff3
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-07 14:19:54 +0330

    on click actions for cart and search

--- app/(front)/ui/MenuItems.jsx
+++ app/(front)/ui/MenuItems.jsx
@@ -1,10 +1,18 @@
+"use client";
 import Link from "next/link";
 // UI
 import { FaSearch } from "react-icons/fa";
 import { CiUser } from "react-icons/ci";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { RiShoppingBasketLine } from "react-icons/ri";
 
-function MenuItems({ color }) {
+function MenuItems({
+  color,
+  setShowCart,
+  showCart,
+  setShowSearch,
+  showSearch,
+}) {
   const menuItems = [
     {
       title: "Home",
@@ -24,7 +32,8 @@ function MenuItems({ color }) {
     },
     {
       title: <FaSearch />,
-      link: "/",
+      // link: "/",
+      id: "search",
     },
     {
       title: <CiUser />,
@@ -34,19 +43,54 @@ function MenuItems({ color }) {
       title: <MdOutlineSpaceDashboard />,
       link: "/dashboard",
     },
+    {
+      title: <RiShoppingBasketLine />,
+      // link: "/dashboard",
+      id: "cart",
+    },
   ];
+  const handleCart = (e) => {
+    setShowCart(!showCart);
+  };
+  const handleSearch = () => {
+    setShowSearch(!showSearch);
+  };
   return (
     <>
-      {menuItems.map((item, i) => (
-        <Link
-          key={i}
-          href={item.link}
-          style={{ color: `${color}` }}
-          className="font-semi bold text-[19px] h-full border-b-2 border-transparent hover:border-b-2 hover:border-white"
-        >
-          {item.title}
-        </Link>
-      ))}
+      {menuItems.map((item, i) =>
+        item.link ? (
+          <Link
+            id={item.id ? item.id : ""}
+            key={i}
+            href={item.link}
+            style={{ color: `${color}` }}
+            className="font-semi bold text-[19px] h-full border-b-2 border-transparent hover:border-b-2 hover:border-white"
+          >
+            {item.title}
+          </Link>
+        ) : (
+          // Render a different tag (replace with your desired tag)
+          <button
+            id={item.id ? item.id : ""}
+            className="font-semi bold text-[19px] h-full border-b-2 border-transparent hover:border-b-2 hover:border-white"
+            key={`${i}-no-link`}
+            style={{ color: `${color}` }}
+            onClick={
+              item.id
+                ? () => {
+                    if (item.id === "cart") {
+                      handleCart();
+                    } else if (item.id === "search") {
+                      handleSearch();
+                    }
+                  }
+                : null
+            }
+          >
+            {item.title}
+          </button>
+        )
+      )}
     </>
   );
 }
