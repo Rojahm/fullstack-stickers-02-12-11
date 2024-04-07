f53f40bd3c337b402579c670cb165123c1a3a4e8
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-07 14:19:33 +0330

    heavy load!

--- app/(front)/ui/Header.jsx
+++ app/(front)/ui/Header.jsx
@@ -1,12 +1,46 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Nav from "./Nav";
 import Hamburger from "./Hamburger";
 import NavDrawer from "./NavDrawer";
+import Cart from "./Cart";
+import Search from "./Search";
 
 function Header({ color }) {
   const [showDrawer, setShowDrawer] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+  // Event listener for html
+  const closeMenuOnBodyClick = (e) => {
+    // get the event path
+    const target = e.composedPath();
+    if (
+      !target.some(
+        (elem) =>
+          elem.id === "drawerNavigation" ||
+          elem.id === "onPageCart" ||
+          elem.id === "hamburger" ||
+          elem.id === "cart" ||
+          elem.id === "search" ||
+          elem.id === "onPageSearch"
+      )
+    ) {
+      // Close the drawerNavigation if any other element other than drawerNavigation is Cliked
+
+      setShowDrawer(false);
+      setShowCart(false);
+      setShowSearch(false);
+    }
+  };
+  // Only add EventListener if the drawerNavigation is open
+  useEffect(() => {
+    // Select html elemet to add event listener
+    const html = document.documentElement;
+    if (showDrawer) {
+      html.addEventListener("click", closeMenuOnBodyClick);
+    }
+  }, [showDrawer]);
   return (
     <div className="z-10 relative flex justify-between items-center pt-3 px-3">
       <Link href={"/"}>
@@ -18,7 +52,14 @@ function Header({ color }) {
         </h1>
       </Link>
 
-      <Nav color={color} setShowDrawer={setShowDrawer} />
+      <Nav
+        color={color}
+        setShowDrawer={setShowDrawer}
+        setShowCart={setShowCart}
+        showCart={showCart}
+        setShowSearch={setShowSearch}
+        showSearch={showSearch}
+      />
       <Hamburger
         color={color}
         setShowDrawer={setShowDrawer}
@@ -29,6 +70,8 @@ function Header({ color }) {
         showDrawer={showDrawer}
         setShowDrawer={setShowDrawer}
       />
+      <Cart showCart={showCart} />
+      <Search showSearch={showSearch} />
     </div>
   );
 }
