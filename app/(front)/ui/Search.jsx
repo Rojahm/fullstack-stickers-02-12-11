5e61c3e8190544f8c09fea1b91459ee25e8178f1
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-07 22:08:23 +0330

    search

--- app/(front)/ui/Search.jsx
+++ app/(front)/ui/Search.jsx
@@ -1,6 +1,8 @@
 "use client";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import { useRouter } from "next/navigation";
 // import { useDebouncedCallback } from "use-debounce";
 
 const searchPack = async (query) => {
@@ -17,7 +19,7 @@ const searchSticker = async (query) => {
 };
 const searchTag = async (query) => {
   const tagData = await fetch(
-    `${process.env.NEXT_PUBLIC_SRV_URL}/searchSticker/${query}`
+    `${process.env.NEXT_PUBLIC_SRV_URL}/getStickersByTag/${query}`
   );
   return tagData.json();
 };
@@ -30,6 +32,7 @@ function Search({ showSearch, setShowSearch }) {
   const [packResult, setPackResult] = useState([]);
   const [stickerResult, setStickerResult] = useState([]);
   const [tagResult, setTagResult] = useState([]);
+  const [result, setResult] = useState([]);
   const handleQuery = (e) => {
     setQuery(e.target.value);
   };
@@ -40,11 +43,24 @@ function Search({ showSearch, setShowSearch }) {
         setPackResult(pack.result);
         const sticker = await searchSticker(query);
         setStickerResult(sticker.result);
+        const tag = await searchTag(query);
+        setTagResult(tag);
+        setResult(pack.result.concat(sticker.result, tag));
+      } else {
+        setPackResult([]);
+        setStickerResult([]);
+        setTagResult([]);
+        setResult([]);
       }
     };
     handleSearch(query);
   }, [query]);
-
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(query);
+    router.push("/search");
+  };
   return (
     <>
       {showSearch ? (
@@ -53,7 +69,7 @@ function Search({ showSearch, setShowSearch }) {
           className="flex justify-center absolute top-10 left-0 w-full"
         >
           <div className="bg-white/85 shadow-xl rounded-md w-[50%] flex flex-col gap-2">
-            <form className="flex">
+            <form className="flex" onSubmit={handleSubmit}>
               <button className="relative px-2 text-[purple] bg-white rounded-tl-md">
                 <IoIosSearch />
               </button>
@@ -74,9 +90,64 @@ function Search({ showSearch, setShowSearch }) {
               </button>
             </form>
 
-            <h1 className="text-[purple] px-4">Search Results</h1>
+            <h1 className="text-[purple] px-4">
+              Search Results:{query ? result.length : null}
+            </h1>
 
-            <div className="px-4 mb-4">Results</div>
+            <div className="px-4 mb-4">
+              {packResult.length > 0 ? (
+                <>
+                  <p className="font-bold">Sticker Packs</p>
+                  <hr className="border-[#814997] border-[1px]" />
+                  <div className="flex flex-col my-2">
+                    {packResult.slice(0, 4).map((pack, i) => (
+                      <Link
+                        key={i + "pack"}
+                        href={"/"}
+                        className="text-zinc-500 hover:font-bold transition-all duration-200 ease-in-out"
+                      >
+                        {pack.title}
+                      </Link>
+                    ))}
+                  </div>
+                </>
+              ) : null}
+              {stickerResult.length > 0 ? (
+                <>
+                  <p className="font-bold">Stickers</p>
+                  <hr className="border-[#814997] border-[1px]" />
+                  <div className="flex flex-col my-2">
+                    {stickerResult.slice(0, 4).map((sticker, i) => (
+                      <Link
+                        key={i}
+                        href={"/"}
+                        className="text-zinc-500 hover:font-bold transition-all duration-200 ease-in-out"
+                      >
+                        {sticker.title}
+                      </Link>
+                    ))}
+                  </div>
+                </>
+              ) : null}
+              {tagResult.length > 0 ? (
+                <>
+                  <p className="font-bold">Tags</p>
+                  <hr className="border-[#814997] border-[1px]" />
+                  <div className="flex flex-col my-2">
+                    {tagResult.slice(0, 4).map((tag, i) => (
+                      <Link
+                        key={i}
+                        href={"/"}
+                        className="text-zinc-500 hover:font-bold transition-all duration-200 ease-in-out"
+                      >
+                        {tag.title}
+                      </Link>
+                    ))}
+                  </div>
+                </>
+              ) : null}
+            </div>
+            {result.length > 0 ? <button>show more</button> : null}
           </div>
         </div>
       ) : null}
