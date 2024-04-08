d2afe0becab5d8d5505eb7e3305e1833ae5deb20
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-08 11:24:16 +0330

    get the text

--- app/(front)/sticker-packs/PackPageHero.js
+++ app/(front)/sticker-packs/PackPageHero.js
@@ -1,21 +1,15 @@
 import Header from "../ui/Header";
 
-function PackPageHero({ title }) {
+function PackPageHero({ title, text }) {
   return (
-    <div className="bg-hero-background-image flex flex-col bg-cover min-h-[200px] h-auto relative">
+    <div className="bg-hero-background-image flex flex-col bg-cover min-h-[200px] h-auto relative w-full">
       <Header color={"white"} />
       <div className="flex flex-col justify-center items-center my-20 pb-14 mx-32 lg:mx-44 gap-10 lg:gap-16">
         <h1 className="text-white font-extrabold text-3xl md:text-[40px] lg:text-[60px] uppercase drop-shadow-md">
           {title}
         </h1>
         <div className="text-center">
-          <p className="text-white text-xl font-bold">
-            Sticker Mania’s custom stickers are totally unlike anything you’ve
-            ever seen before. They’re made, shared and remixed by the Sticker
-            Mania fam, so Sticker Mania’s the only place you’ll find them. Oh
-            also? They’re 100% free and ALL up for grabs for you to play with.
-            So what are you waiting for?
-          </p>
+          <p className="text-white text-xl font-bold">{text}</p>
         </div>
       </div>
 
