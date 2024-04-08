d5ebb141c04b3648ec4be7168e2119271d17b56a
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-08 13:41:48 +0330

    update on query change

--- app/(front)/search/PackResult.jsx
+++ app/(front)/search/PackResult.jsx
@@ -3,11 +3,12 @@ import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import { searchPack } from "@/app/lib/data";
 import Packs from "@/app/(front)/ui/Packs";
-function PackResult() {
+function PackResult({ setTitle }) {
   const params = useSearchParams();
   const query = params.get("query");
   const [packs, setPacks] = useState([]);
   useEffect(() => {
+    setTitle(query);
     const handleSearch = async (query) => {
       if (query) {
         const packResult = await searchPack(query);
@@ -17,7 +18,7 @@ function PackResult() {
       }
     };
     handleSearch(query);
-  }, []);
+  }, [query]);
   return (
     <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-5">
       <Packs packs={packs} />
