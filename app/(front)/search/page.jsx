e7ac99b1b738313a4702092294d011d2348fd156
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-08 13:42:30 +0330

    set the title state in one of subcomponent that access use search param

--- app/(front)/search/page.jsx
+++ app/(front)/search/page.jsx
@@ -9,6 +9,8 @@ import { useState, useEffect } from "react";
 import PackResult from "./PackResult";
 
 function SearchPage() {
+  const [title, setTitle] = useState("");
+
   // const [tags, setTags] = useState([]);
   // useEffect(() => {
   //   const handleSearch = async (query) => {
@@ -35,17 +37,17 @@ function SearchPage() {
   // }, []);
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <PackPageHero title={`results for --`} />
+      <PackPageHero title={`results for ${title}`} />
       {/* Breadcrumbs here */}
       <div className="flex gap-2 leading-6 text-[22px] font-semibold w-[90%] lg:w-[80%]">
         <Link href={"/"}>Home</Link>
-        <div className={"text-[#814997] capitalize"}>/ results for --</div>
+        <div className={"text-[#814997] capitalize"}>/ results for {title}</div>
       </div>
       {/* List of Sticker Packs */}
       <div className="w-[90%] lg:w-[80%] my-10 flex flex-col gap-5">
         <Title title={"Sticker Packs"} link={"/sticker-packs"} />
         <Suspense fallback={<Loading />}>
-          <PackResult />
+          <PackResult setTitle={setTitle} />
         </Suspense>
         <Title title={"Stickers"} link={"/stickers"} />
         <Suspense fallback={<Loading />}>
