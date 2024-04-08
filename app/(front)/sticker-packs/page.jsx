e5cb55ff13689039fdd30ab405bd7e647f870fc4
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-08 11:24:34 +0330

    use title component

--- app/(front)/sticker-packs/page.jsx
+++ app/(front)/sticker-packs/page.jsx
@@ -5,11 +5,17 @@ import PackPageHero from "./PackPageHero";
 // import StickersPacks from "./StickersPacks";
 const StickersPacks = lazy(() => import("./StickersPacks"));
 import Loading from "@/app/Loading";
+import Title from "../ui/Title";
 
 function StickerPacksPage() {
+  const text = `Sticker Mania’s custom stickers are totally unlike anything you’ve
+            ever seen before. They’re made, shared and remixed by the Sticker
+            Mania fam, so Sticker Mania’s the only place you’ll find them. Oh
+            also? They’re 100% free and ALL up for grabs for you to play with.
+            So what are you waiting for?`;
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <PackPageHero title={"ALL STICKER PACKS"} />
+      <PackPageHero title={"ALL STICKER PACKS"} text={text} />
       {/* Breadcrumbs here */}
       <div className="flex gap-2 leading-6 text-[22px] font-semibold w-[90%] lg:w-[80%]">
         <Link href={"/"}>Home</Link>
@@ -19,10 +25,7 @@ function StickerPacksPage() {
       </div>
       {/* List of Sticker Packs */}
       <div className="w-[90%] lg:w-[80%] my-10">
-        <Link href={"/"} className="text-4xl">
-          Sticker Packs
-        </Link>
-        <hr className="border-[#814997] border-[3px] rounded-md" />
+        <Title title={"Sticker Packs"} link={"/sticker-packs"} />
         <Suspense fallback={<Loading />}>
           <StickersPacks />
         </Suspense>
