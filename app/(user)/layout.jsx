3c0084bb00f281f0d1db5fb9f2bb207ac50510d9
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-10 22:00:27 +0330

    handle layouts

--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -1,10 +1,7 @@
-import { Nunito } from "next/font/google";
 import Footer from "@/app/(front)/ui/Footer";
 import Header from "@/app/(front)/ui/Header";
 import "@/app/globals.css";
 import UserNav from "@/app/(user)/ui/UserNav";
-
-const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Profile | ChopStick",
@@ -13,21 +10,17 @@ export const metadata = {
 
 export default function ProfileLayout({ children }) {
   return (
-    <html lang="en" className={`${nunito.className} antialiased`}>
-      <body>
-        <Header color={"black"} />
-        <div className="flex flex-col justify-center items-center w-full">
-          <div className="flex justify-center gap-5 p-5 w-full lg:w-[70%]">
-            <div className="bg-sky-100/60 rounded-lg">
-              <UserNav />
-            </div>
-            <div className="w-full bg-sky-100/60 p-5 rounded-lg">
-              {children}
-            </div>
+    <section>
+      <Header color={"black"} />
+      <div className="flex flex-col justify-center items-center w-full">
+        <div className="flex justify-center gap-5 p-5 w-full lg:w-[70%]">
+          <div className="bg-sky-100/60 rounded-lg">
+            <UserNav />
           </div>
+          <div className="w-full bg-sky-100/60 p-5 rounded-lg">{children}</div>
         </div>
-        <Footer />
-      </body>
-    </html>
+      </div>
+      <Footer />
+    </section>
   );
 }
