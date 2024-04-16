f56f255023565b5f1b64a1a2159a8c22f3dff17f
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-04-16 16:07:31 +0330

    verifying user

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,8 +1,65 @@
+"use client";
+import { useState } from "react";
+import { useSignUp } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Header from "@/app/(front)/ui/Header";
-import { SignUp } from "@clerk/nextjs";
+// import { SignUp } from "@clerk/nextjs";
 
 function SignUpPage() {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [pendingVerification, setPendingVerification] = useState(false);
+  const [code, setCode] = useState("");
+  const router = useRouter();
+  // start the sign up process.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        emailAddress,
+        password,
+      });
+
+      // send the email.
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      // change the UI to our pending section.
+      setPendingVerification(true);
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  // This verifies the user using email code that is delivered.
+  const onPressVerify = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+      if (completeSignUp.status !== "complete") {
+        /*  investigate the response, to see if there was an error
+         or if the user needs to complete more steps.*/
+        console.log(JSON.stringify(completeSignUp, null, 2));
+      }
+      if (completeSignUp.status === "complete") {
+        await setActive({ session: completeSignUp.createdSessionId });
+        router.push("/");
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
   return (
     <div className="bg-hero-background-image flex flex-col bg-cover h-[100vh] relative">
       <Header color={"white"} />
@@ -11,16 +68,52 @@ function SignUpPage() {
           Sign Up
         </h1>
         <div className="text-center w-[80%] lg:w-[40%]">
-          <div className="flex text-center justify-center items-center">
+          {/* <div className="flex text-center justify-center items-center">
             <SignUp />
-          </div>
-          {/* <form className="flex flex-col gap-5">
-            <input type="text" className="rounded p-2"></input>
-            <input type="password" className="rounded p-2"></input>
-            <button className="bg-orange-400 text-white rounded py-2 font-bold">
-              Sign Up
-            </button>
-          </form> */}
+          </div> */}
+          {!pendingVerification && (
+            <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  onChange={(e) => setEmailAddress(e.target.value)}
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="rounded p-2"
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="rounded p-2"
+                ></input>
+              </div>
+              <button
+                // onClick={handleSubmit}
+                type="submit"
+                className="bg-orange-400 text-white rounded py-2 font-bold"
+              >
+                Sign Up
+              </button>
+            </form>
+          )}
+          {pendingVerification && (
+            <div>
+              <form>
+                <input
+                  value={code}
+                  placeholder="Code..."
+                  onChange={(e) => setCode(e.target.value)}
+                />
+                <button onClick={onPressVerify}>Verify Email</button>
+              </form>
+            </div>
+          )}
           <div className="flex justify-center items-center gap-5 my-5">
             <p className="text-white">Already Have an account?</p>
             <Link
@@ -39,3 +132,46 @@ function SignUpPage() {
 }
 
 export default SignUpPage;
+
+// export default function SignUpForm() {
+
+//   return (
+//     <div>
+//       {!pendingVerification && (
+//         <form>
+//           <div>
+//             <label htmlFor="email">Email</label>
+//             <input
+//               onChange={(e) => setEmailAddress(e.target.value)}
+//               id="email"
+//               name="email"
+//               type="email"
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="password">Password</label>
+//             <input
+//               onChange={(e) => setPassword(e.target.value)}
+//               id="password"
+//               name="password"
+//               type="password"
+//             />
+//           </div>
+//           <button onClick={handleSubmit}>Sign up</button>
+//         </form>
+//       )}
+//       {pendingVerification && (
+//         <div>
+//           <form>
+//             <input
+//               value={code}
+//               placeholder="Code..."
+//               onChange={(e) => setCode(e.target.value)}
+//             />
+//             <button onClick={onPressVerify}>Verify Email</button>
+//           </form>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
---
+++ app/verify-sign-up/page.jsx
@@ -0,0 +1,10 @@
+function VerifyPage() {
+  return (
+    <div>
+      3 sign in 4 auth different menu based on roles route access role based for
+      admin and member sign in and sign out button custom place
+    </div>
+  );
+}
+
+export default VerifyPage;
