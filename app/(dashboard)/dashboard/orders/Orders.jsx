f50daec186d743a3eb20b4effd81f10fa8c47400
Rojahm <100220824+Rojahm@users.noreply.github.com>
2024-05-05 15:09:56 +0330

    responsive view of orders list in dashboard

--- app/(dashboard)/dashboard/orders/Orders.jsx
+++ app/(dashboard)/dashboard/orders/Orders.jsx
@@ -14,69 +14,74 @@ import Link from "next/link";
 function Orders() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [usersInfo, setUsersInfo] = useState([]);
-  const [usersQty, setUsersQty] = useState();
+  const [ordersInfo, setOrdersInfo] = useState([]);
+  const [ordersQty, setOrdersQty] = useState();
   const pagenumber = Number(searchParams.get("pn")) || 1;
   const pagination = Number(searchParams.get("pg")) || 15;
   useEffect(() => {
     axios
       .get(
-        `${process.env.NEXT_PUBLIC_SRV_URL}/users?pn=${pagenumber}&pg=${pagination}`
+        `${process.env.NEXT_PUBLIC_SRV_URL}/orders?pn=${pagenumber}&pg=${pagination}`
       )
       .then((res) => {
-        setUsersInfo(res.data.paginatedUsers);
-        setUsersQty(res.data.qty);
+        setOrdersInfo(res.data.paginatedOrders);
+        setOrdersQty(res.data.qty);
       });
   }, []);
 
   // Delete User
-  const deleteUser = (id) => {
+  const deleteOrder = (id) => {
     toast.promise(
       axios
-        .post(`${process.env.NEXT_PUBLIC_SRV_URL}/deleteUser/${id}`)
+        .post(`${process.env.NEXT_PUBLIC_SRV_URL}/deleteOrder/${id}`)
         .then((res) => console.log(res.data.msg))
         .catch((err) => console.log(err)),
       {
         pending: "Deleting",
-        success: "User Deleted",
+        success: "Order Deleted",
         error: "error",
       }
     );
     router.refresh();
   };
+  console.log(ordersInfo);
   return (
-    <div className="w-[80%]">
+    <div className="w-[80%] flex flex-col justify-center items-center">
       <ToastContainer />
-      <hr className="min-w-96" />
-      {usersInfo
-        ? usersInfo.map((user, i) => (
+      {ordersInfo
+        ? ordersInfo.map((order, i) => (
             <div key={i}>
-              <div className="border-x-2 min-w-96 hover:border-[purple] p-4 gap-10 flex justify-between">
+              <hr className="min-w-40" />
+              <div className="border-x-2 min-w-40 hover:border-[purple] p-4 gap-10 flex justify-between">
                 <Link
-                  href={`/dashboard/users/${user.user_id}`}
-                  className="flex flex-wrap justify-between w-full gap-2"
+                  href={`/dashboard/orders/${order._id}`}
+                  className="grid grid-cols-4 items-center w-fit gap-2"
                 >
-                  <div className="text-xs text-[purple]">{user.user_id}</div>
-                  <div className="font-semibold text-[purple]">{user.name}</div>
-                  <div className="font-semibold text-[purple]">
-                    {user.lastname}
+                  <div className="text-xs text-[purple] text-ellipsis overflow-hidden">
+                    {order._id}
                   </div>
-                  <div className="font-semibold text-[purple]">
-                    {user.email}
+                  <div className="font-semibold text-[purple] text-ellipsis overflow-hidden">
+                    {order.status}
+                  </div>
+                  <div className="font-semibold text-[purple] text-ellipsis overflow-hidden">
+                    {order.price}
+                  </div>
+                  <div className="font-semibold text-[purple] text-ellipsis overflow-hidden">
+                    {order.user_id}
                   </div>
                 </Link>
                 <div className="flex gap-2">
-                  <button onClick={() => deleteUser(user._id)}>
+                  <button onClick={() => deleteOrder(order._id)}>
                     <FaRegTrashAlt />
                   </button>
                 </div>
               </div>
-              <hr className="min-w-96" />
+              <hr className="min-w-40" />
             </div>
           ))
         : null}
       {/* pagination */}
-      <Pagination qty={usersQty} pn={pagenumber} pg={pagination} />
+      <Pagination qty={ordersQty} pn={pagenumber} pg={pagination} />
     </div>
   );
 }
